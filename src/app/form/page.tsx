e712158406f68e80549eb7cb614ad16516b1c839
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2024-02-12 20:18:28 +0530

    feat : studentForm

---
+++ src/app/form/page.tsx
@@ -0,0 +1,13 @@
+import StudentForm from "@/components/forms/StudentForm";
+
+const formPage = () => {
+  return (
+    <div className="">
+      <div>
+        <StudentForm />
+      </div>
+    </div>
+  );
+};
+
+export default formPage;
---
+++ src/components/forms/StudentForm.tsx
@@ -0,0 +1,180 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "../ui/textarea";
+
+const StudentForm = () => {
+  const formSchema = z.object({
+    name: z.string(),
+    department: z.string(),
+    gender: z.string(),
+    category: z.string(),
+    batch: z.string(),
+    address: z.string(),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      department: "",
+      gender: "",
+      category: "",
+      batch: "",
+      address: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <div className="mx-auto w-1/2">
+      <h1 className="p-2 text-3xl font-bold">Student Form</h1>
+      <div className="p-2">
+        <p className="font-light">
+          Fill in the form below to register a new student
+        </p>
+      </div>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Name" {...field} />
+                </FormControl>
+                <FormDescription>enter your full name here</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="department"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>department</FormLabel>
+                <FormControl>
+                  <Input placeholder="Department" {...field} />
+                </FormControl>
+                <FormDescription>Specify your department here</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="gender"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Gender</FormLabel>
+                <FormControl>
+                  <Input placeholder="Gender" {...field} />
+                </FormControl>
+                <FormDescription>
+                  Specify your Gender Male or Female
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="category"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Category</FormLabel>
+                <FormControl>
+                  <Select onValueChange={field.onChange}>
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Category" {...field} />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Select the category</SelectLabel>
+                        <SelectItem value="SDE">SDE</SelectItem>
+                        <SelectItem value="Fullstack">Fullstack</SelectItem>
+                        <SelectItem value="cyberSecurity">
+                          cyberSecurity
+                        </SelectItem>
+                        <SelectItem value="General">General</SelectItem>
+                        <SelectItem value="HigherStudies">
+                          HigherStudies
+                        </SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormDescription></FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="batch"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Batch</FormLabel>
+                <FormControl>
+                  <Input type="number" placeholder="Batch" {...field} />
+                </FormControl>
+                <FormDescription>Specify your batch here</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="address"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Address</FormLabel>
+                <FormControl>
+                  <Textarea placeholder="Enter Your Address" {...field} />
+                </FormControl>
+                <FormDescription></FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default StudentForm;
