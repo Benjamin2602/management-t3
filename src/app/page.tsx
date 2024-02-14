fc299d607d583c3dc03595b764d8805a54d01c41
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2024-02-14 09:12:20 +0530

    💄 feat : home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,42 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 export default async function Home() {
-  return <div>hello world</div>;
+  return (
+    <div className="m-10">
+      <h1 className=" flex items-center justify-center text-5xl font-bold">
+        Student Management
+      </h1>
+      <section className="mx-16 my-4">
+        <div className="my-12 grid grid-cols-1 items-center md:grid-cols-2 ">
+          <div className="">
+            <div className="font-display flex flex-col justify-center  gap-2 text-6xl font-medium">
+              <span className="tracking-tight">Remain up-to-date</span>
+              <span className="tracking-tight">With current data</span>
+            </div>
+
+            <p className="pt-4 text-2xl">
+              My website provides you with the tools to track <br />
+              student statistics in seconds.
+            </p>
+            <Link href="/form" className="flex gap-4 py-10 xl:pt-10">
+              <Button className=" border-4 border-indigo-400/90 bg-primary p-6 text-xl font-light text-white shadow-md transition-all duration-300  hover:scale-110 hover:shadow-indigo-300">
+                Getting Started
+              </Button>
+            </Link>
+          </div>
+          <div>
+            <video
+              className="rounded-3xl shadow-md shadow-neutral-600"
+              autoPlay
+              muted
+              loop
+            >
+              <source src="/content/dataAnal.mp4" type="video/mp4" />
+            </video>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
